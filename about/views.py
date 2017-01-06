cfa26ea200848c8f25cd66e17291480ac84eed19
Saisanthosh Mamidala <santhoshmamidala@unm.edu>
2017-01-06 10:11:49 -0800

    About template

--- about/views.py
+++ about/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def first(request):
-    return HttpResponse("Rango says 'Here is about page'.<br/><a href='/RangoApp/'>Index</a>")
+    #return HttpResponse("Rango says 'Here is about page'.<br/><a href='/RangoApp/'>Index</a>")
+    context_dict = {'message': "This is about page."}
+    return render(request, 'RangoApp/about.html', context_dict)
